1e0707bf74f23428aeab9d44ca914e49020c4b02
M-ADARSHCHARY <adarshchary.05@gmail.com>
2025-11-21 11:00:32 +0530

    deployment issue

--- server/src/utils/generateToken.js
+++ server/src/utils/generateToken.js
@@ -8,7 +8,7 @@ export const generateToken = (user, res) => {
 
     return res.cookie("token", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
+        secure: true,
         sameSite:"none",
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
     });
